b54de02f1118bdb52e69b4c3acfba67685aff13c
murilio <murilio.nssw@gmail.com>
2022-09-20 23:00:21 -0300

    finalizando a base da API

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  return res.json({ message: 'Hello' })
+  return res.json({ message: 'Hello 1' })
 })
 
 app.listen(3333)
