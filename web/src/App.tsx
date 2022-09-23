091b0ec11ae3e247ee664c86fb4879e3d37b82d6
murilio <murilio.nssw@gmail.com>
2022-09-22 22:32:45 -0300

    configurando o tailwind e o eslint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,12 @@
+import './styles/main.css'
+
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img className='' src="https://random.imagecdn.app/200/200" alt="" />
+
+    </div>
   )
 }
 
 export { App }
-
