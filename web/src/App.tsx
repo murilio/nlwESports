b46d317e118ae381bb45ff3c9fd263261fd69642
murilio <murilio.nssw@gmail.com>
2022-09-23 09:53:59 -0300

    finalizando a landing page web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,80 @@
 import './styles/main.css'
 
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 const App = () => {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img className='' src="https://random.imagecdn.app/200/200" alt="" />
 
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="https://random.imagecdn.app/500/500" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
